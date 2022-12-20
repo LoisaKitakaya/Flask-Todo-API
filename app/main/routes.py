e3589f639f82a6d0e9902bd3076adccc39a08f64
Freedom Loisa <kitakayaloisa@gmail.com>
2022-12-20 14:23:18 +0300

    frontend design

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,3 +5,28 @@ from . import bp
 def index():
 
     return render_template('index.html')
+
+@bp.route('/about/')
+def about():
+
+    return render_template('about.html')
+
+@bp.route('/contact/', methods=['GET', 'POST'])
+def contact():
+
+    return render_template('contact.html')
+
+@bp.route('/signup/', methods=['GET', 'POST'])
+def signup():
+
+    return render_template('signup.html')
+
+@bp.route('/login/', methods=['GET', 'POST'])
+def login():
+
+    return render_template('login.html')
+
+@bp.route('/generate_token/', methods=['POST'])
+def generate_token():
+
+    pass
