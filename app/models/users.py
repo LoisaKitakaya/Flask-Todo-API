9e05676a03fe3b70eab3b33ade830a687ca4416f
Freedom Loisa <kitakayaloisa@gmail.com>
2022-12-20 12:52:52 +0300

    modified user model

--- app/models/users.py
+++ app/models/users.py
@@ -5,8 +5,10 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(256), unique=True)
     username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(100), unique=True)
+    verified = db.Column(db.Boolean, default=False)
     password = db.Column(db.String(254))
     todos = db.relationship('todo.Todo', backref='user')
 
